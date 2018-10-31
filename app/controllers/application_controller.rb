6b0b927f92bd5628a635d93cad5c45b8b9307cf3
Katrina Pritchard <pritckat@gmail.com>
2018-10-31 19:05:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  
+
   def logged_in?
     !!current_user
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action: require_logged_in, except:[:new, :create]
 
   def new
     @user = User.new
